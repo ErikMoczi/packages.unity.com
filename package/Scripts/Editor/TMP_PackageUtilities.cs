c2dd3125da25574ce440fb893678a5e25620f79c
ErikMoczi <32465761+ErikMoczi@users.noreply.github.com>
2018-09-02 15:58:17 +0000

    1.0.17 - 2017/11/20 @2018.1

--- package/Scripts/Editor/TMP_PackageUtilities.cs
+++ package/Scripts/Editor/TMP_PackageUtilities.cs
@@ -18,7 +18,7 @@ namespace TMPro
         }
 
         /// <summary>
-        /// 
+        /// Data structure containing the target and replacement fileIDs and GUIDs which will require remapping from previous version of TextMesh Pro to the new TextMesh Pro UPM package.
         /// </summary>
         [System.Serializable]
         private struct AssetConversionRecord
@@ -29,9 +29,11 @@ namespace TMPro
         }
 
         /// <summary>
-        /// 
+        /// Data structure containing a list of target and replacement fileID and GUID requiring remapping from previous versions of TextMesh Pro to the new TextMesh Pro UPM package.
+        /// This data structure is populated with the data contained in the PackageConversionData.json file included in the package.
         /// </summary>
         [System.Serializable]
+        #pragma warning disable 0649
         private class AssetConversionData
         {
             public List<AssetConversionRecord> assetRecords;
@@ -46,11 +48,16 @@ namespace TMPro
             public string assetDataFile;
         }
 
-        private string m_ProjectScanResults = string.Empty;
-        private Vector2 m_ProjectScanResultScrollPosition;
+        private static string k_ProjectScanReportDefaultText = "<color=#FFFF80><b>Project Scan Results</b></color>\n";
+        private static string m_ProjectScanResults = string.Empty;
+        private static Vector2 m_ProjectScanResultScrollPosition;
+        private static float m_ProgressPercentage = 0;
+        private static List<AssetModificationRecord> m_ModifiedAssetList = new List<AssetModificationRecord>();
+
         private static SerializationMode m_ProjectAssetSerializationMode;
         private static string m_ProjectExternalVersionControl;
-        private static float m_ProgressPercentage = 0;
+
+
 
         void OnEnable()
         {
@@ -60,8 +67,9 @@ namespace TMPro
             // Get the UI Skin and Styles for the various Editors
             TMP_UIStyleManager.GetUIStyles();
 
-            m_ProjectScanResults = "<color=#FFFF00><b>Project Scan Results</b></color>\n";
+            m_ProjectScanResults = k_ProjectScanReportDefaultText;
         }
+
 
         void OnGUI()
         {
@@ -72,39 +80,50 @@ namespace TMPro
                 GUILayout.Label("<b>TMP Project Files GUID Remapping Tool</b>", TMP_UIStyleManager.Section_Label);
 
                 // Scan project files and resources
-                EditorGUILayout.BeginVertical(TMP_UIStyleManager.SquareAreaBox85G);
-                GUILayout.Label("<b>Scan Project Files</b>", TMP_UIStyleManager.Label);
-                GUILayout.Label("To begin scanning your project for files & resources that were created with a previous version of TextMesh Pro, please click the button below.", TMP_UIStyleManager.Label);
-
-                GUILayout.Space(5f);
-
-                if (GUILayout.Button("Begin Scanning Project Files"))
+                GUILayout.BeginVertical(TMP_UIStyleManager.SquareAreaBox85G);
                 {
-                    m_ProjectScanResults = ScanProjectFiles();
+                    GUILayout.Label("<b>Scan Project Files</b>", TMP_UIStyleManager.Label);
+                    GUILayout.Label("Press the <i>Scan Project Files</i> button to begin scanning your project for files & resources that were created with a previous version of TextMesh Pro", TMP_UIStyleManager.Label);
+                    GUILayout.Space(5f);
 
-                }
+                    if (GUILayout.Button("Scan Project Files"))
+                    {
+                        m_ProjectScanResults = ScanProjectFiles();
+                    }
 
-                Rect rect = GUILayoutUtility.GetRect(0f, 20f, GUILayout.ExpandWidth(true));
-                EditorGUI.ProgressBar(rect, m_ProgressPercentage / 100, "Scan Progress");
+                    // Display progress bar
+                    Rect rect = GUILayoutUtility.GetRect(0f, 20f, GUILayout.ExpandWidth(true));
+                    EditorGUI.ProgressBar(rect, m_ProgressPercentage / 100, "Scan Progress");
+                    GUILayout.Space(5);
 
-                // Creation Feedback
-                GUILayout.Space(5);
-                GUILayout.BeginVertical(TMP_UIStyleManager.TextAreaBoxWindow);
-                {
-                    m_ProjectScanResultScrollPosition = EditorGUILayout.BeginScrollView(m_ProjectScanResultScrollPosition, GUILayout.Height(145));
-                    EditorGUILayout.LabelField(m_ProjectScanResults, TMP_UIStyleManager.Label);
-                    EditorGUILayout.EndScrollView();
+                    // Creation Feedback
+                    GUILayout.BeginVertical(TMP_UIStyleManager.TextAreaBoxWindow);
+                    {
+                        m_ProjectScanResultScrollPosition = EditorGUILayout.BeginScrollView(m_ProjectScanResultScrollPosition, GUILayout.Height(145));
+                        EditorGUILayout.LabelField(m_ProjectScanResults, TMP_UIStyleManager.Label);
+                        EditorGUILayout.EndScrollView();
+                    }
+                    GUILayout.EndVertical();
+                    GUILayout.Space(5f);
                 }
                 GUILayout.EndVertical();
 
-                //GUILayout.Label("The relative path to a Resources folder where the Font Assets and Material Presets are located.\nExample \"Fonts & Materials/\"", TMP_UIStyleManager.Label);
-                //EditorGUILayout.PropertyField(prop_DefaultFontAssetPath, new GUIContent("Path:        Resources/"));
-                EditorGUILayout.EndVertical();
+                // Scan project files and resources
+                GUILayout.BeginVertical(TMP_UIStyleManager.SquareAreaBox85G);
+                { 
+                    GUILayout.Label("<b>Save Modified Project Files</b>", TMP_UIStyleManager.Label);
+                    GUILayout.Label("Pressing the <i>Save Modified Project Files</i> button will update the project files listed in the <i>Project Scan Results</i> above. <color=#FFFF80>Please make sure that you have created a backup of your project first</color> as these file modifications are permanent and cannot be undone.", TMP_UIStyleManager.Label);
+                    GUILayout.Space(5f);
 
-                // Scan project to identify files and resources to be converted to new TextMesh Pro UPM package format.
+                    GUI.enabled = m_ModifiedAssetList.Count > 0 ? true : false;
+                    if (GUILayout.Button("Save Modified Project Files"))
+                    {
+                        UpdateProjectFiles();
+                    }
+                    GUILayout.Space(10f);
+                }
+                GUILayout.EndVertical();
 
-
-                // List project files and resources that have been identified for conversion.
             }
             GUILayout.EndVertical();
         }
@@ -124,7 +143,7 @@ namespace TMPro
 
             Vector2 currentWindowSize = editorWindow.minSize;
 
-            editorWindow.minSize = new Vector2(Mathf.Max(640, currentWindowSize.x), Mathf.Max(300, currentWindowSize.y));
+            editorWindow.minSize = new Vector2(Mathf.Max(640, currentWindowSize.x), Mathf.Max(418, currentWindowSize.y));
         }
 
         private static string ScanProjectFiles()
@@ -136,8 +155,8 @@ namespace TMPro
             string packageFullPath = EditorUtilities.TMP_EditorUtility.packageFullPath;
 
             // List containing assets that have been modified.
-            string scanResults = "<color=#FFFF00><b>Project Scan Results</b></color>\n";
-            List<AssetModificationRecord> modifiedAssetList = new List<AssetModificationRecord>();
+            string scanResults = k_ProjectScanReportDefaultText;
+            m_ModifiedAssetList.Clear();
             m_ProgressPercentage = 0;
 
             // Read Conversion Data from Json file.
@@ -154,12 +173,21 @@ namespace TMPro
                 string assetFilePath = AssetDatabase.GUIDToAssetPath(guid);
                 System.Type assetType = AssetDatabase.GetMainAssetTypeAtPath(assetFilePath);
 
-                // Filter out file types we are not interested in
+                // Filter out asset types that we can't read or have not interest in searching.
                 if (assetType == typeof(DefaultAsset) || assetType == typeof(MonoScript) || assetType == typeof(Texture2D) || assetType == typeof(TextAsset) || assetType == typeof(Shader))
                     continue;
 
                 // Read the asset data file
-                string assetDataFile = File.ReadAllText(projectPath + "/" + assetFilePath);
+                string assetDataFile = string.Empty;
+                try
+                {
+                    assetDataFile = File.ReadAllText(projectPath + "/" + assetFilePath);
+                }
+                catch
+                {
+                    // Continue to the next asset if we can't read the current one.
+                    continue;
+                }
 
                 //Debug.Log("Searching Asset: [" + assetFilePath + "] of type: " + assetType);
 
@@ -185,7 +213,7 @@ namespace TMPro
                     modifiedAsset.assetFilePath = assetFilePath;
                     modifiedAsset.assetDataFile = assetDataFile;
 
-                    modifiedAssetList.Add(modifiedAsset);
+                    m_ModifiedAssetList.Add(modifiedAsset);
 
                     scanResults += assetFilePath + "\n";
                 }
@@ -228,33 +256,39 @@ namespace TMPro
                     modifiedAsset.assetFilePath = assetMetaFilePath;
                     modifiedAsset.assetDataFile = assetMetaFile;
 
-                    modifiedAssetList.Add(modifiedAsset);
+                    m_ModifiedAssetList.Add(modifiedAsset);
 
                     scanResults += assetFilePath + "\n";
                 }
             }
 
+            return scanResults;
+        }
+
+        private static void UpdateProjectFiles()
+        {
+            // Make sure Asset Serialization mode is set to ForceText with Visible Meta Files.
+            SetProjectSerializationAndSourceControlModes();
+
             // Display dialogue to show user a list of project files that will be modified upon their consent.
-            //if (EditorUtility.DisplayDialog("Save Modified Asset(s)?", "Are you sure you want to save all modified assets?", "YES", "NO"))
-            //{
-            //    for (int i = 0; i < modifiedAssetList.Count; i++)
-            //    {
-            //        // Make sure all file streams that might have been opened by Unity are closed.
-            //        AssetDatabase.UnloadAllFileStreams();
+            if (EditorUtility.DisplayDialog("Save Modified Asset(s)?", "Are you sure you want to save all modified assets?", "YES", "NO"))
+            {
+                for (int i = 0; i < m_ModifiedAssetList.Count; i++)
+                {
+                    // Make sure all file streams that might have been opened by Unity are closed.
+                    AssetDatabase.UnloadAllFileStreams();
 
-            //        Debug.Log("Writing asset file [" + modifiedAssetList[i].assetFilePath + "].");
+                    Debug.Log("Writing asset file [" + m_ModifiedAssetList[i].assetFilePath + "].");
 
-            //        //File.WriteAllText(projectPath + "/" + modifiedAssetList[i].assetFilePath, modifiedAssetList[i].assetDataFile);
-            //    }
-
-            //}
+                    //File.WriteAllText(projectPath + "/" + modifiedAssetList[i].assetFilePath, modifiedAssetList[i].assetDataFile);
+                }
+            }
 
             AssetDatabase.Refresh();
 
             // Restore project Asset Serialization and Source Control modes.
             RestoreProjectSerializationAndSourceControlModes();
 
-            return scanResults;
         }
 
         /// <summary>
@@ -329,7 +363,7 @@ namespace TMPro
 
 
         // Create Sprite Asset Editor Window
-        [MenuItem("Window/TextMeshPro/Convert TMP Project Files to UPM", false, 1510)]
+        //[MenuItem("Window/TextMeshPro/Convert TMP Project Files to UPM", false, 1510)]
         public static void ConvertProjectGUIDsMenu()
         {
             ConvertProjectGUIDsToUPM();
@@ -368,10 +402,8 @@ namespace TMPro
 
         private static void GetVersionInfo()
         {
-            string file = Path.GetFullPath("Assets/TextMesh Pro/Plugins/TextMeshPro.dll");
-
-            Debug.Log(System.Diagnostics.FileVersionInfo.GetVersionInfo(file).FileVersion);
-            //Debug.Log(System.Diagnostics.FileVersionInfo.GetVersionInfo(file).ProductName);
+            string version = TMP_Settings.version;
+            Debug.Log("The version of this TextMesh Pro UPM package is (" + version + ").");
         }
 
 
