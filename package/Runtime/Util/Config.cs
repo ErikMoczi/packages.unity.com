029393253f53d45719b22b4d1cf9ad02d2bbfa4c
ErikMoczi <32465761+ErikMoczi@users.noreply.github.com>
2018-09-02 16:13:29 +0000

    1.4.2 - 2018/01/06 @2018.1

--- package/Runtime/Util/Config.cs
+++ package/Runtime/Util/Config.cs
@@ -16,38 +16,38 @@ namespace ResourceManagement.Util
 
             var className = var.Substring(0, i);
             var propName = var.Substring(i + 1);
-            foreach (var a in AppDomain.CurrentDomain.GetAssemblies())
-            {
-                Type t = a.GetType(className, false, false);
-                if (t == null)
-                    continue;
-                try
-                {
-                    var pi = t.GetProperty(propName, System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.FlattenHierarchy | System.Reflection.BindingFlags.Public);
-                    if (pi != null)
-                    {
-                        var v = pi.GetValue(null, null);
-                        if (v != null)
-                        {
-                            cachedValues.Add(var, v.ToString());
-                            return v.ToString();
-                        }
-                    }
-                    var fi = t.GetField(propName, System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.FlattenHierarchy | System.Reflection.BindingFlags.Public);
-                    if (fi != null)
-                    {
-                        var v = fi.GetValue(null);
-                        if (v != null)
-                        {
-                            cachedValues.Add(var, v.ToString());
-                            return v.ToString();
-                        }
-                    }
-                }
-                catch (Exception)
-                {
-                }
-            }
+            //foreach (var a in AppDomain.CurrentDomain.GetAssemblies())
+            //{
+            //    Type t = a.GetType(className, false, false);
+            //    if (t == null)
+            //        continue;
+            //    try
+            //    {
+            //        var pi = t.GetProperty(propName, System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.FlattenHierarchy | System.Reflection.BindingFlags.Public);
+            //        if (pi != null)
+            //        {
+            //            var v = pi.GetValue(null, null);
+            //            if (v != null)
+            //            {
+            //                cachedValues.Add(var, v.ToString());
+            //                return v.ToString();
+            //            }
+            //        }
+            //        var fi = t.GetField(propName, System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.FlattenHierarchy | System.Reflection.BindingFlags.Public);
+            //        if (fi != null)
+            //        {
+            //            var v = fi.GetValue(null);
+            //            if (v != null)
+            //            {
+            //                cachedValues.Add(var, v.ToString());
+            //                return v.ToString();
+            //            }
+            //        }
+            //    }
+            //    catch (Exception)
+            //    {
+            //    }
+            //}
             return var;
         }
 
