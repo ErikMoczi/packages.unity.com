561da9068e53056f7f9d22b75d2bb8395c2ace63
ErikMoczi <32465761+ErikMoczi@users.noreply.github.com>
2018-09-02 16:20:33 +0000

    1.3.0 - 2018/02/02 @2018.1

--- package/Editor/AssetBundleBrowserMain.cs
+++ package/Editor/AssetBundleBrowserMain.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-[assembly: System.Runtime.CompilerServices.InternalsVisibleToAttribute("Unity.AssetBundleBrowser.EditorTests")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleToAttribute("Unity.AssetBundleBrowser.Editor.Tests")]
 
 namespace AssetBundleBrowser
 {
