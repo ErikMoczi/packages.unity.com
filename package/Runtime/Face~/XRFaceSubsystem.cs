a9f339f088877450e6adc328f42ce1c6ad2bccc7
ErikMoczi <32465761+ErikMoczi@users.noreply.github.com>
2018-09-03 11:16:03 +0000

    1.0.0-preview.2 - 2018/05/23 @2018.1

---
+++ package/Runtime/Face~/XRFaceSubsystem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnityEngine.Experimental.XR
+{
+    public struct FaceAddedEventArgs
+    {
+        public XRFaceSubsystem FaceSubsystem { get; internal set; }
+        public XRFace Face { get; internal set; }
+    }
+
+    public struct FaceUpdatedEventArgs
+    {
+        public XRFaceSubsystem FaceSubsystem { get; internal set; }
+        public XRFace Face { get; internal set; }
+    }
+
+    public struct FaceRemovedEventArgs
+    {
+        public XRFaceSubsystem FaceSubsystem { get; internal set; }
+        public XRFace Face { get; internal set; }
+    }
+
+    public class XRFaceSubsystem : Subsystem<XRFaceSubsystemDescriptor>
+    {
+        public void GetAllFaces(List<XRFace> facesOut)
+        {
+            if (facesOut == null)
+                throw new ArgumentException("facesOut");
+
+            GetAllFacesAsList(facesOut);
+        }
+
+        void GetAllFacesAsList(List<XRFace> faces)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
