35eebbff5f7f29cf0faee2729ffa7ee3b5c807f3
ErikMoczi <32465761+ErikMoczi@users.noreply.github.com>
2018-09-03 10:51:31 +0000

    1.0.2-preview - 2018/06/22 @2018.1

---
+++ package/Tests/Runtime/TriangleTest.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+public class TriangleTest {
+
+    [Test]
+    public void PlaceHolderTest()
+    {
+        // Use the Assert class to test conditions.
+        Assert.Pass("This is a placeholder to ensure we have at least one editor test.");
+    }
+}
