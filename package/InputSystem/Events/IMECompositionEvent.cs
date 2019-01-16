6ae30fde0b9fb27d8a904470edc2ef9745a4185d
ErikMoczi <32465761+ErikMoczi@users.noreply.github.com>
2019-01-16 09:34:34 +0000

    0.0.9-preview - 2018/10/09 @2018.2

--- package/InputSystem/Events/IMECompositionEvent.cs
+++ package/InputSystem/Events/IMECompositionEvent.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using UnityEngine.Experimental.Input.LowLevel;
 using UnityEngine.Experimental.Input.Utilities;
 
 namespace UnityEngine.Experimental.Input.LowLevel
@@ -9,7 +12,7 @@ namespace UnityEngine.Experimental.Input.LowLevel
     /// This event contains the entire current string to date, and once a new composition is submitted will send a blank string event.
     /// </summary>
     [StructLayout(LayoutKind.Explicit, Size = InputEvent.kBaseEventSize + sizeof(int) + (sizeof(char) * kIMECharBufferSize))]
-    public unsafe struct IMECompositionEvent : IInputEventTypeInfo
+    public struct IMECompositionEvent : IInputEventTypeInfo
     {
         // These needs to match the native ImeCompositionStringInputEventData settings
         public const int kIMECharBufferSize = 64;
@@ -19,7 +22,7 @@ namespace UnityEngine.Experimental.Input.LowLevel
         public InputEvent baseEvent;
 
         [FieldOffset(InputEvent.kBaseEventSize)]
-        public IMEComposition composition;
+        public IMECompositionString compositionString;
 
         public FourCC GetTypeStatic()
         {
@@ -30,8 +33,8 @@ namespace UnityEngine.Experimental.Input.LowLevel
 
 namespace UnityEngine.Experimental.Input
 {
-    [StructLayout(LayoutKind.Explicit, Size = sizeof(int) + (sizeof(char) * LowLevel.IMECompositionEvent.kIMECharBufferSize))]
-    public unsafe struct IMEComposition
+    [StructLayout(LayoutKind.Explicit, Size = sizeof(int) + sizeof(char) * IMECompositionEvent.kIMECharBufferSize)]
+    public unsafe struct IMECompositionString : IEnumerable<char>
     {
         public int Count
         {
@@ -41,12 +44,12 @@ namespace UnityEngine.Experimental.Input
             }
         }
 
-        public unsafe char this[int index]
+        public char this[int index]
         {
             get
             {
                 if (index >= Count || index < 0)
-                    throw new IndexOutOfRangeException();
+                    throw new ArgumentOutOfRangeException("index");
 
                 fixed(char* ptr = buffer)
                 {
@@ -56,9 +59,78 @@ namespace UnityEngine.Experimental.Input
         }
 
         [FieldOffset(0)]
-        int size;
+        internal int size;
 
         [FieldOffset(sizeof(int))]
-        fixed char buffer[LowLevel.IMECompositionEvent.kIMECharBufferSize];
+        internal fixed char buffer[IMECompositionEvent.kIMECharBufferSize];
+
+        public override string ToString()
+        {
+            fixed(char* ptr = buffer)
+            return new string(ptr);
+        }
+
+        public IEnumerator<char> GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        internal struct Enumerator : IEnumerator<char>
+        {
+            IMECompositionString m_CompositionString;
+            char m_CurrentCharacter;
+            int m_CurrentIndex;
+
+            public Enumerator(IMECompositionString compositionString)
+            {
+                m_CompositionString = compositionString;
+                m_CurrentCharacter = '\0';
+                m_CurrentIndex = -1;
+            }
+
+            public bool MoveNext()
+            {
+                var size = m_CompositionString.Count;
+
+                m_CurrentIndex++;
+
+                if (m_CurrentIndex == size)
+                    return false;
+
+                fixed(char* ptr = m_CompositionString.buffer)
+                {
+                    m_CurrentCharacter = *(ptr + m_CurrentIndex);
+                }
+
+                return true;
+            }
+
+            public void Reset()
+            {
+                m_CurrentIndex = -1;
+            }
+
+            public void Dispose()
+            {
+            }
+
+            public char Current
+            {
+                get
+                {
+                    return m_CurrentCharacter;
+                }
+            }
+
+            object IEnumerator.Current
+            {
+                get { return Current; }
+            }
+        }
     }
 }
