6dd102b439c07b2b796c23d3243a4eb660b61bb8
ErikMoczi <32465761+ErikMoczi@users.noreply.github.com>
2019-03-08 15:44:11 +0000

    0.0.7-preview.8 - 2019/03/08 @2019.1

--- package/Unity.Jobs/IJobParallelForDeferExtensions.cs
+++ package/Unity.Jobs/IJobParallelForDeferExtensions.cs
@@ -9,30 +9,54 @@ namespace Unity.Jobs
 #if !UNITY_ZEROPLAYER
     public static class IJobParallelForDeferExtensions
     {
-        internal struct ParallelForJobStruct<T> where T : struct, IJobParallelFor
+        internal struct JobStructNativeList<T> where T: struct, IJobParallelFor
         {
-            public static IntPtr                            jobReflectionData;
+            public T JobData;
+
+            public static IntPtr jobReflectionData;
 
             public static IntPtr Initialize()
             {
                 if (jobReflectionData == IntPtr.Zero)
-                {
-                    var attribute = (JobProducerTypeAttribute)typeof(IJobParallelFor).GetCustomAttribute(typeof(JobProducerTypeAttribute));
-                    var jobStruct = attribute.ProducerType.MakeGenericType(typeof(T));
-                    var method = jobStruct.GetMethod("Initialize", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
-                    var res = method.Invoke(null, new object[0]);
-                    jobReflectionData = (IntPtr) res;
-                }
-                    
+                    jobReflectionData = JobsUtility.CreateJobReflectionData(typeof(JobStructNativeList<T>), typeof(T), JobType.ParallelFor, (ExecuteJobFunction)Execute);
+
                 return jobReflectionData;
             }
+
+            public delegate void ExecuteJobFunction(ref JobStructNativeList<T> data, System.IntPtr additionalPtr, System.IntPtr bufferRangePatchData, ref JobRanges ranges, int jobIndex);
+
+            public unsafe static void Execute(ref JobStructNativeList<T> self, System.IntPtr additionalPtr, System.IntPtr bufferRangePatchData, ref JobRanges ranges, int jobIndex)
+            {
+                while (true)
+                {
+                    int begin;
+                    int end;
+                    if (!JobsUtility.GetWorkStealingRange(ref ranges, jobIndex, out begin, out end))
+                        break;
+
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+                    JobsUtility.PatchBufferMinMaxRanges(bufferRangePatchData, UnsafeUtility.AddressOf(ref self.JobData), begin, end - begin);
+#endif
+
+                    for (var i = begin; i < end; ++i)
+                        self.JobData.Execute(i);
+                }
+
+            }
+
         }
 
         unsafe public static JobHandle Schedule<T, U>(this T jobData, NativeList<U> list, int innerloopBatchCount, JobHandle dependsOn = new JobHandle()) 
             where T : struct, IJobParallelFor 
             where U : struct
         {
-            var scheduleParams = new JobsUtility.JobScheduleParameters(UnsafeUtility.AddressOf(ref jobData), ParallelForJobStruct<T>.Initialize(), dependsOn, ScheduleMode.Batched);
+            var fullData = new JobStructNativeList<T>
+            {
+                JobData = jobData
+            };
+
+            var scheduleParams = new JobsUtility.JobScheduleParameters(UnsafeUtility.AddressOf(ref fullData), JobStructNativeList<T>.Initialize(), dependsOn, ScheduleMode.Batched);
+
             void* atomicSafetyHandlePtr = null;
 #if ENABLE_UNITY_COLLECTIONS_CHECKS
             var safety = NativeListUnsafeUtility.GetAtomicSafetyHandle(ref list);
