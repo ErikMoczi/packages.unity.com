d813fadf020533a8177f0f5061a92c73259d659e
ErikMoczi <32465761+ErikMoczi@users.noreply.github.com>
2019-03-08 16:55:34 +0000

    0.1.0-preview.2 - 2019/02/22 @2018.1

--- package/CHANGELOG.md
+++ package/CHANGELOG.md
@@ -4,7 +4,15 @@ All notable changes to this package will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
-## [0.1.0] - 2018-10-08
+## [0.1.0-preview.2] - 2019.02.22
+
+### Rebuild meta files.
+
+## [0.1.0-preview.1] - 2019-02-01
+
+### Move samples outside of main package.
+
+## [0.1.0-preview.0] - 2018-10-08
 
 ### This is the first release of *Unity Package Settings Manager*.
 
---
+++ package/Samples~/Editor/MySettingsProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityEditor.SettingsManagement.Examples
+{
+	static class MySettingsProvider
+	{
+		const string k_PreferencesPath = "Preferences/My Settings";
+
+#if UNITY_2018_3_OR_NEWER
+		[SettingsProvider]
+		static SettingsProvider CreateSettingsProvider()
+		{
+			var provider = new UserSettingsProvider(k_PreferencesPath,
+				MySettingsManager.instance,
+				new [] { typeof(MySettingsProvider).Assembly });
+
+			return provider;
+		}
+#else
+
+	[NonSerialized]
+	static UserSettingsProvider s_SettingsProvider;
+
+	[PreferenceItem("ProBuilder")]
+	static void ProBuilderPreferencesGUI()
+	{
+		if (s_SettingsProvider == null)
+			s_SettingsProvider = new UserSettingsProvider(MySettingsManager.instance, new[] { typeof(MySettingsProvider).Assembly });
+
+		s_SettingsProvider.OnGUI(null);
+	}
+
+#endif
+	}
+}
