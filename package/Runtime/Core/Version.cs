1746041177177b7593a6f9fb7740feb269e172b7
ErikMoczi <32465761+ErikMoczi@users.noreply.github.com>
2019-03-14 20:19:28 +0000

    4.0.4 - 2019/03/14 @2018.3

--- package/Runtime/Core/Version.cs
+++ package/Runtime/Core/Version.cs
@@ -5,7 +5,7 @@ namespace UnityEngine.ProBuilder
     /// </summary>
     static class Version
     {
-        internal static readonly SemVer currentInfo = new SemVer("4.0.4-preview.11", "2019/03/14");
+        internal static readonly SemVer currentInfo = new SemVer("4.0.4", "2019/03/13");
 
         /// <summary>
         /// Get the current version.
